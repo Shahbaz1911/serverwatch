82db7bde773d25e71224bacf957492ea9dbb9138
Shahbaz Khan <shahbazkhan19117866@gmail.com>
2025-10-05 12:37:34 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/api/netdata/route.ts
+++ src/app/api/netdata/route.ts
@@ -32,7 +32,7 @@ export async function GET(request: NextRequest) {
     // Netdata system.ram provides 'free', 'used', 'cached', 'buffers'. We want percentage of used.
     const ramUsed = ramData?.data?.[0]?.[ramData.dimension_names.indexOf('used')] ?? 0;
     const ramFree = ramData?.data?.[0]?.[ramData.dimension_names.indexOf('free')] ?? 0;
-    const ramCached = ramData?.data?_.[0]?.[ramData.dimension_names.indexOf('cached')] ?? 0;
+    const ramCached = ramData?.data?.[0]?.[ramData.dimension_names.indexOf('cached')] ?? 0;
     const ramBuffers = ramData?.data?.[0]?.[ramData.dimension_names.indexOf('buffers')] ?? 0;
     const totalRam = ramUsed + ramFree + ramCached + ramBuffers;
     const ram = totalRam > 0 ? (ramUsed / totalRam) * 100 : 0;
